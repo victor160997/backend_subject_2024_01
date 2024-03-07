7f5e6a109b24597967edcbbfb8cb966120c78ec5
victor.rodrigues <victor.rodrigues@radixeng.com.br>
2024-03-07 09:09:33 -0300

    first commit

---
+++ calculadora/src/main/java/com/example/calculadora/resources/CalcResource.java
@@ -0,0 +1,28 @@
+package com.example.calculadora.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calc")
+public class CalcResource {
+    @GetMapping("/imc")
+    public String imc(@RequestParam double peso, @RequestParam double altura) {
+        double imc = peso / altura * altura;
+        if (imc < 18.5) {
+            return "Abaixo do peso";
+        } else if (imc < 24.9) {
+            return "Peso normal";
+        } else if (imc < 29.9) {
+            return "Sobrepeso";
+        } else if (imc < 34.9) {
+            return "Obesidade grau 1";
+        } else if (imc < 39.9) {
+            return "Obesidade grau 2";
+        } else {
+            return "Obesidade grau 3";
+        }
+    }
+}
