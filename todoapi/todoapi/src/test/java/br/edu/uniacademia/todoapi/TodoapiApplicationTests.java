2be7a31ddde314e8bfa62417ae1e6e611363243a
victor.rodrigues <victor.rodrigues@radixeng.com.br>
2024-03-07 21:10:42 -0300

    inicializando todoapi

---
+++ todoapi/todoapi/src/test/java/br/edu/uniacademia/todoapi/TodoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.uniacademia.todoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
